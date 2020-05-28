5f986f29555b30340149fe1681877738570820d5
Alexander Molchevsky <molchevsky@gmail.com>
2020-05-28 11:27:00 +0300

    Two solutions finished

--- min_moves_to_make_string_without_3_identical_consecutive_letters/main.cpp
+++ min_moves_to_make_string_without_3_identical_consecutive_letters/main.cpp
@@ -2,18 +2,33 @@
 #include <string>
 
 using namespace std;
+/*
+ To solve this task we need to find sequences of the same letters and if the sequence is longer than 3
+ divide length of this sequence to 3 and add result of the division to counters of needed swaps
 
-int solution(string S) {
-    int wl = 0, wr = 0, ans = 0;
-    while (wr < S.size()) {
-        if (S[wl] == S[wr]) ++wr;
-        else {
-            ans += (wr - wl) / 3;
-            wl = wr;
-        }
+ Example of work:
+3 consecutive : baaab, replace the middle a (3 / 3 == 1)
+4 consecutive : baaaab, replace the third a (4 / 3 == 1)
+5 consecutive : baaaaab, replace the third a (5 / 3 == 1)
+6 consecutive : baaaaaab -> baabaaab -> baaab -> bab with 2 replancement (6 / 3 == 2)
+10 consecutive : baaaaaaaaaab -> baabaaaaaaab -> baaaaaaab -> baaaab -> baab with 3 replacement (10 / 3 == 3)
+
+therefore, we can see the rule, counter += (consecutive char number) / 3
+ */
+
+int solution(const string & s){
+    int res = 0, s_size = s.size();
+    for(int i = 0; i < s_size;) {
+        int next = i + 1;
+        // if we meet sequence of the same letters
+        // scanning the string to find length of this sequence
+        while( (next < s_size) && (s[i] == s[next]) ) { next++; }
+        // Here "next - i" is length of the sequence
+        // Each third letter should be changed to remove too long sequences
+        res += (next - i) / 3;
+        i = next; // skip processed letters
     }
-    ans += (wr - wl) / 3;
-    return ans;
+    return res;
 }
 
 int main() {
--- numbers_with_equal_digit_sum/main.cpp
+++ numbers_with_equal_digit_sum/main.cpp
@@ -3,17 +3,8 @@
 #include <unordered_map>
 
 using namespace std;
-/*
-int dsum(int a) {
-    int s = 0;
-    while (a > 0) {
-        s += a % 10;
-        a = a / 10;
-    }
-    return s;
-}
-*/
 
+// this function returns sum of digits of a given number
 int dsum(int a) {
     int s = 0;
     for (; a > 0; a /= 10) {
@@ -22,20 +13,30 @@ int dsum(int a) {
     return s;
 }
 
-int solution(const vector<int> & A) {
-    unordered_map<int, int> m;
-    int ans = 0;
-    for (auto a : A) {
-        int s = dsum(a);
+int solution(const vector<int> & v) {
+    using sum_t = int;
+    using number_t = int;
+    unordered_map<sum_t, number_t> m;
+    // Maximum sum of given numbers
+    int max_sum = 0;
+    for (auto i : v) {
+        int s = dsum(i);
+        // If we have no such sum of digits in the map
         if (!m.count(s)) {
-            m[s] = a;
+            // add this sum to the map as a key and the number as a value
+            m[s] = i;
         }
         else {
-            ans = max(ans, m[s] + a);
-            m[s] = max(a, m[s]);
+            // if we have the sum of digits in the map
+            // if sum of current number and number from the map with the same sum of digits are bigger than
+            // maximum encountered sum then update the maximum sum
+            max_sum = max(max_sum, m[s] + i);
+            // Save current number as value for current sum of digits if it is bigger than number from the map
+            m[s] = max(i, m[s]);
         }
     }
-    return ans ? ans : -1;
+    // if the are no numbers with equal sum return -1
+    return max_sum ? max_sum : -1;
 }
 
 int main() {
