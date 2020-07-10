67e8f7f145ad0c5c19ac83d4293feb081dd5b6db
Alexander Molchevsky <molchevsky@gmail.com>
2020-07-10 09:28:04 +0300

    solutions updated

--- min_steps_to_make_piles_equal_height/main.cpp
+++ min_steps_to_make_piles_equal_height/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <unordered_set>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(const vector<int> & v) {
-    return unordered_set(v.begin(), v.end()).size();
+    int v_size = v.size();
+    int dif = unordered_set(v.begin(), v.end()).size();
+    return v_size == dif ? dif : (v_size-1) * (dif-1);
 }
 
 int main() {
 
     cout << solution({5,2,1}) << " Expected: 3" << endl;
+    cout << solution({5,5,1}) << " Expected: 2" << endl;
+    cout << solution({5,5,5,5,1}) << " Expected: 4" << endl;
 
     return 0;
 }
