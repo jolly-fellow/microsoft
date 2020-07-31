6e8822402380e9576236b64e4ec4ec9e2b489e7f
Alexander Molchevsky <molchevsky@gmail.com>
2020-08-01 00:15:42 +0300

    solutions updated

--- largest_alphabetic_character/README.md
+++ largest_alphabetic_character/README.md
@@ -1,2 +1,5 @@
 # microsoft
-Solutions for known Microsoft online assessment tasks 
+
+https://leetcode.com/discuss/interview-question/548119/
+
+Given a string S, find the largest alphabetic character, whose both uppercase and lowercase appear in S. The uppercase character should be returned. For example, for S = "admeDCAB", return "D". If there is no such character, return "NO". 
--- largest_alphabetic_character/main.cpp
+++ largest_alphabetic_character/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
+char safe_tolower(char ch) {
+    return static_cast<char>(std::tolower(static_cast<unsigned char>(ch)));
+}
+
+string solution (const string & s) {
+    char max_char = 0;
+    string res("NO");
+    unordered_set<char> us(s.begin(), s.end());
+
+    for(auto c : s) {
+        if(isupper(c)) {
+            if(us.count(safe_tolower(c))) {
+                max_char = max(c, max_char);
+            }
+        }
+    }
+
+    if(max_char) { res = max_char; }
+
+    return res;
+
+
+}
+
 int main() {
 
-    cout << "" << endl;
+    cout << solution("admeDCAB") << " Expected: D" << endl;
 
     return 0;
 }
